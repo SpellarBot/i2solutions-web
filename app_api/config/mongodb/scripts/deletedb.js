7ca3e3b08d8b9b04608e458594668d048fb739fc
Joel Rodriguez <joelerll@gmail.com>
2018-01-17 18:18:17 -0500

    update limpiar base de datos mongo

--- app_api/config/mongodb/scripts/deletedb.js
+++ app_api/config/mongodb/scripts/deletedb.js
@@ -8,30 +8,32 @@ const dbName = process.env.MONGO_DB_NAME
 MongoClient.connect(url, (err, client) => {
   assert.equal(null, err)
   console.log('Connected successfully to server')
-
   const db = client.db(dbName)
-  db.collection('novedades').remove({}, function (err, numberRemoved) {
-    if (err) {
-      throw new Error(`Ha ocurrido un error ${err}`)
-    }
-    console.log('inside remove call back' + numberRemoved)
-  })
-  db.collection('puestos').remove({}, function (err, numberRemoved) {
-    console.log('inside remove call back' + numberRemoved)
-    if (err) {
-      throw new Error(`Ha ocurrido un error ${err}`)
-    }
-  })
-  db.collection('puestos_detalle').remove({}, function (err, numberRemoved) {
-    console.log('inside remove call back' + numberRemoved)
-    if (err) {
-      throw new Error(`Ha ocurrido un error ${err}`)
-    }
-  })
-  db.collection('counters').remove({}, function (err, numberRemoved) {
-    console.log('inside remove call back' + numberRemoved)
-    if (err) {
-      throw new Error(`Ha ocurrido un error ${err}`)
-    }
-  })
+  Promise.all([
+    LimpiarColeccion('novedades', db),
+    LimpiarColeccion('puestos', db),
+    LimpiarColeccion('puestos_detalle', db),
+    LimpiarColeccion('counters', db)
+    ])
+    .then((values) => {
+      console.log(values)
+      client.close()
+    }).catch((err) => {
+      console.log(err)
+    })
 })
+
+
+function LimpiarColeccion(nombreColeccion, db) {
+  return new Promise((resolve, reject) => {
+    db.collection(nombreColeccion).remove({}, function (err, numberRemoved) {
+      console.log('inside remove call back' + numberRemoved)
+      if (err) {
+        throw new Error(`Ha ocurrido un error ${err}`)
+        reject(nombreColeccion)
+      } else {
+        resolve(nombreColeccion)
+      }
+    })
+  })
+}
