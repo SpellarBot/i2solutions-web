7875e1f43e410dca371573c1c2d31f896d1d09f3
Alex Ferrin <alex-710@hotmail.com>
2018-07-08 18:12:22 -0500

    Agregar Dialogs y Cards para Puestos, Accidentes, Capacitaciones y Personas. Aún no hay opción de editar ni nada, solo mostrar

---
+++ client/src/components/CardPuestos.vue
@@ -0,0 +1,42 @@
+<template>
+  <main id="CardPuestos">
+    <v-card>
+      <h3>Empaquetado de Pollo</h3>
+      <p> Puesto donde se realiza el empaquetado de pollo y se envía a distribución.</p>
+      <span class="link" v-on:click="visualizarPersonas"> Número Personas: 1</span>
+      <span class="link" v-on:click="visualizarAccidentes"> Número Accidentes: 1</span>
+      <span class="link"> Novedades sin arender: 1</span>
+    </v-card>
+    <footer>
+      <DialogPersonasFromPuestos
+    :visible="visiblePersonas"
+    @close="visiblePersonas=false"
+    ></DialogPersonasFromPuestos>
+    <DialogAccidentesFromPuestos
+    :visible="visibleAccidentes"
+    @close="visibleAccidentes=false"
+    ></DialogAccidentesFromPuestos>
+    </footer>
+  </main>
+</template>
+<script>
+import DialogPersonasFromPuestos from './DialogPersonasFromPuestos'
+import DialogAccidentesFromPuestos from './DialogAccidentesFromPuestos'
+export default {
+  components: { DialogPersonasFromPuestos, DialogAccidentesFromPuestos },
+  data () {
+    return {
+      visiblePersonas: false,
+      visibleAccidentes: false
+    }
+  },
+  methods: {
+    visualizarPersonas () {
+      this.visiblePersonas = true
+    },
+    visualizarAccidentes () {
+      this.visibleAccidentes = true
+    }
+  }
+}
+</script>
---
+++ client/src/components/DialogCapacitacionesFromEstablecimientos.vue
@@ -0,0 +1,52 @@
+<template>
+  <main id="DialogCapacitacionesFromEstablecimientos">
+    <v-dialog fullscreen v-model="show" @keydown.esc="show=false" hide-overlay transition="dialog-bottom-transition">
+      <v-card>
+      <v-toolbar dark color="primary">
+        <v-btn icon dark @click.native="show = false">
+          <v-icon>close</v-icon>
+        </v-btn>
+        <v-toolbar-title>Establecimiento Matriz</v-toolbar-title>
+      </v-toolbar>
+      <h1>Capacitaciones: </h1>
+      <v-layout>
+        <v-flex xs12 sm4 offset-sm4>
+        <v-card class='mb-4'>
+          <CardCapacitaciones></CardCapacitaciones>
+          <div><b>Área de trabajo:</b> Producción 1</div>
+        </v-card>
+        <v-card class='mb-4'>
+          <CardCapacitaciones></CardCapacitaciones>
+          <div><b>Área de trabajo:</b> Producción 2</div>
+        </v-card>
+      </v-flex>
+      </v-layout>
+    </v-card>
+    </v-dialog>
+  </main>
+</template>
+<script>
+import CardCapacitaciones from './CardCapacitaciones'
+export default {
+  components: { CardCapacitaciones },
+  name: 'DialogPuestos',
+  props: ['visible'],
+  computed: {
+    show: {
+      get () {
+        return this.visible
+      },
+      set (value) {
+        if (!value) {
+          this.$emit('close')
+        }
+      }
+    }
+  }
+}
+</script>
+<style>
+.izq {
+  text-align: left;
+}
+</style>
